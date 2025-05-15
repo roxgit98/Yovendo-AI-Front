e34aa517512aeac93ac9132b5d58547207409742
Davide Giliberti <gilo.davide@hotmail.it>
2025-05-15 17:05:48 +0200

    modale aggiunto

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Modal,
+  Row,
+} from "react-bootstrap";
 import { Gear, PersonFill } from "react-bootstrap-icons";
 
 function Home() {
   const messages = [{ message: "ciao" }, { message: "come va?" }];
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
       <Container>
         {messages.map((msg, index) => (
-          <Row className="d-flex flex-row-reverse justify-content-center mb-1">
-            <Col key={index} sm={2}>
+          <Row
+            key={index}
+            className="d-flex flex-row-reverse justify-content-center mb-1"
+          >
+            <Col sm={2}>
               <Card className="mt-5 bg-secondary">
                 <Card.Body>
                   <Card.Text>{msg.message}</Card.Text>
@@ -28,7 +42,11 @@ function Home() {
                   <Gear color="white"></Gear>
                   Impostazioni
                 </Button>
-                <Button variant="outline-secondary" className="mt-3">
+                <Button
+                  variant="outline-secondary"
+                  className="mt-3"
+                  onClick={handleShow}
+                >
                   <PersonFill color="white"></PersonFill>
                   Accedi
                 </Button>
@@ -44,6 +62,24 @@ function Home() {
           </Col>
         </Row>
       </Container>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Inserisci le credenziali</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
